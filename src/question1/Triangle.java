2ada1463f5aafd29d42053a5586ddcb95322f048
AhanafAhamed <ahanafahamed9@gmail.com>
2024-11-06 13:41:41 +0600

    First Commmit

---
+++ src/question1/Triangle.java
@@ -0,0 +1,30 @@
+package question1;
+
+public class Triangle
+{
+    private double side1;
+    private double side2;
+    private double side3;
+
+    Triangle()
+    {
+        this.side1 = 1;
+        this.side2 = 1;
+        this.side3 = 1;
+
+    }
+
+    Triangle(double side1, double side2, double side3)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+
+    }
+
+    public double getArea()
+    {
+        return 0.5 *
+    }
+
+}
